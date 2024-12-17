94646e3d69d171abab479aecd9bf399cca1982d5
FabienGuitton <fabienguittonfr@gmail.com>
2024-12-17 15:07:53 +0100

    Update MCD and DB

--- ConceptionDB/DB.sql
+++ ConceptionDB/DB.sql
@@ -1,12 +1,16 @@
-/* No Finish ! */
+/* SQL for Dominus */
+CREATE DATABASE Dominus;
 
-CREATE TABLE Ingredients(
-   IdIgredient COUNTER,
-   nomIng VARCHAR(100) NOT NULL,
+USE Dominus;
+
+/* Creation of all tables */
+CREATE OR REPLACE TABLE Ingredients(
+   IdIgredient BIGINT AUTO_INCREMENT NOT NULL,
+   nameIng VARCHAR(100) NOT NULL,
    stockIng BIGINT NOT NULL,
-   uniteIng VARCHAR(25) NOT NULL,
-   prixHTIng REAL NOT NULL,
-   prixTTCIng REAL NOT NULL,
+   unityIng VARCHAR(25) NOT NULL,
+   priceHTIng DOUBLE NOT NULL,
+   priceTTCIng DOUBLE NOT NULL,
    userCreate VARCHAR(100) NOT NULL,
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
@@ -14,39 +18,39 @@ CREATE TABLE Ingredients(
    PRIMARY KEY(IdIgredient)
 );
 
-CREATE TABLE Produits(
-   IdProduit COUNTER,
-   nomProduit VARCHAR(100) NOT NULL,
-   tailleProduit VARCHAR(50) NOT NULL,
-   categorieProduit VARCHAR(100) NOT NULL,
-   prixHTProduit REAL NOT NULL,
-   prixTTCProduit REAL NOT NULL,
+CREATE OR REPLACE TABLE Products(
+   IdProduct BIGINT AUTO_INCREMENT NOT NULL,
+   nameProduct VARCHAR(100) NOT NULL,
+   sizeProduct VARCHAR(50) NOT NULL,
+   categoryProduct VARCHAR(100) NOT NULL,
+   priceHTProduct DOUBLE NOT NULL,
+   priceTTCProduct DOUBLE NOT NULL,
    userCreate VARCHAR(100) NOT NULL,
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
    dateModif DATETIME NOT NULL,
-   PRIMARY KEY(IdProduit)
+   PRIMARY KEY(IdProduct)
 );
 
-CREATE TABLE Clients(
-   idClient COUNTER,
-   nomCli VARCHAR(100) NOT NULL,
-   telCli CHAR(10) NOT NULL,
-   numRueCli CHAR(50) NOT NULL,
-   nomRueCli VARCHAR(255) NOT NULL,
-   codePostalCli VARCHAR(50) NOT NULL,
-   instructionCli VARCHAR(100),
-   comInterneCli VARCHAR(100),
+CREATE OR REPLACE TABLE Customers(
+   idCustomer BIGINT AUTO_INCREMENT NOT NULL,
+   nameCst VARCHAR(100) NOT NULL,
+   telCst CHAR(10) NOT NULL,
+   streetNumberCst CHAR(50) NOT NULL,
+   streetNameCst VARCHAR(255) NOT NULL,
+   postcodeCst VARCHAR(50) NOT NULL,
+   instructionsCst VARCHAR(100),
+   InternalComCst VARCHAR(100),
    userCreate VARCHAR(100) NOT NULL,
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
    dateModif DATETIME NOT NULL,
-   PRIMARY KEY(idClient)
+   PRIMARY KEY(idCustomer)
 );
 
-CREATE TABLE Employes(
-   idEmploye COUNTER,
-   nomEmp VARCHAR(100) NOT NULL,
+CREATE OR REPLACE TABLE Employees(
+   idEmployee BIGINT AUTO_INCREMENT NOT NULL,
+   nameEmp VARCHAR(100) NOT NULL,
    codeEmp CHAR(4) NOT NULL,
    roleEmp VARCHAR(50) NOT NULL,
    telEmp CHAR(10) NOT NULL,
@@ -54,94 +58,125 @@ CREATE TABLE Employes(
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
    dateModif DATETIME NOT NULL,
-   PRIMARY KEY(idEmploye),
+   PRIMARY KEY(idEmployee),
    UNIQUE(codeEmp)
 );
 
-CREATE TABLE Pointages(
-   idPointage COUNTER,
-   debutPointage DATETIME NOT NULL,
-   finPointage DATETIME,
+CREATE OR REPLACE TABLE ClockingIn(
+   idClockingIn BIGINT AUTO_INCREMENT NOT NULL,
+   startClockingIn DATETIME NOT NULL,
+   endClockingIn DATETIME,
    userCreate VARCHAR(100) NOT NULL,
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
    dateModif DATETIME NOT NULL,
-   idEmploye INT,
-   PRIMARY KEY(idPointage),
-   FOREIGN KEY(idEmploye) REFERENCES Employes(idEmploye)
+   idEmployee BIGINT,
+   PRIMARY KEY(idClockingIn),
 );
 
-CREATE TABLE Commandes(
-   idCommande COUNTER,
-   nomCmd VARCHAR(50) NOT NULL,
-   typeCmd VARCHAR(50) NOT NULL,
-   payerCmdON LOGICAL NOT NULL,
-   reductionCmd REAL,
-   priseDateCmd DATETIME NOT NULL,
-   pretDateCmd DATETIME NOT NULL,
-   prixHTCmd REAL NOT NULL,
-   prixTTCCmd REAL NOT NULL,
+CREATE OR REPLACE TABLE Discounts(
+   idDiscount BIGINT AUTO_INCREMENT NOT NULL,
+   nameDist VARCHAR(50) NOT NULL,
+   valueDist DOUBLE NOT NULL,
+   codeDist CHAR(6) NOT NULL,
+   AccredDistON BOOLEAN NOT NULL,
    userCreate VARCHAR(100) NOT NULL,
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
    dateModif DATETIME NOT NULL,
-   idClient INT,
+   PRIMARY KEY(idDiscount),
+   UNIQUE(codeDist)
+);
+
+CREATE OR REPLACE TABLE Orders(
+   idCommande BIGINT AUTO_INCREMENT NOT NULL,
+   nameOrd VARCHAR(50) NOT NULL,
+   typeOrd VARCHAR(50) NOT NULL,
+   payOrdON BOOLEAN NOT NULL,
+   reductionOrd DOUBLE,
+   takingDateOrd DATETIME NOT NULL,
+   readyDateOrd DATETIME NOT NULL,
+   priceHTOrd DOUBLE NOT NULL,
+   priceTTCOrd DOUBLE NOT NULL,
+   userCreate VARCHAR(100) NOT NULL,
+   dateCreate DATETIME NOT NULL,
+   userModif VARCHAR(100) NOT NULL,
+   dateModif DATETIME NOT NULL,
+   idDiscount BIGINT,
+   idCustomer BIGINT,
    PRIMARY KEY(idCommande),
-   FOREIGN KEY(idClient) REFERENCES Clients(idClient)
 );
 
-CREATE TABLE LignePanier(
-   IdLignePanier COUNTER,
-   qteProduitLP INT NOT NULL,
-   prixHTLP REAL NOT NULL,
-   prixTTCLP REAL NOT NULL,
+CREATE OR REPLACE TABLE LineBasket(
+   IdLineBasket BIGINT AUTO_INCREMENT NOT NULL,
+   qtyProductLB INT NOT NULL,
+   priceHTLB DOUBLE NOT NULL,
+   priceTTCLB DOUBLE NOT NULL,
    userCreate VARCHAR(100) NOT NULL,
    dateCreate DATETIME NOT NULL,
    userModif VARCHAR(100) NOT NULL,
    dateModif DATETIME NOT NULL,
-   IdProduit INT,
-   idCommande INT,
-   PRIMARY KEY(IdLignePanier),
-   FOREIGN KEY(IdProduit) REFERENCES Produits(IdProduit),
-   FOREIGN KEY(idCommande) REFERENCES Commandes(idCommande)
+   IdProduct BIGINT,
+   idCommande BIGINT,
+   PRIMARY KEY(IdLineBasket),
 );
 
-CREATE TABLE ingredientsParDefault(
-   IdIgredient INT,
-   IdProduit INT,
+/* Declaration of table ralations */
+ALTER TABLE ClockingIn
+   ADD CONSTRAINT FK_EmployeeClokingIn
+   ADD FOREIGN KEY(idEmployee) REFERENCES Employees(idEmployee);
+
+ALTER TABLE Orders
+   ADD CONSTRAINT FK_DiscountOrder
+   FOREIGN KEY(idDiscount) REFERENCES Discounts(idDiscount),
+   ADD CONSTRAINT FK_CustomerOrder
+   FOREIGN KEY(idCustomer) REFERENCES Customers(idCustomer);
+
+ALTER TABLE LineBasket
+   ADD CONSTRAINT FK_ProductLineBasket
+   FOREIGN KEY(IdProduct) REFERENCES Products(IdProduct),
+   ADD CONSTRAINT FK_CommandeLineBasket
+   FOREIGN KEY(idCommande) REFERENCES Orders(idCommande);
+
+/* CREATE N,N ??? */
+/*
+CREATE OR REPLACE TABLE DefaultIngredients(
+   IdIgredient BIGINT,
+   IdProduct BIGINT,
    userCreate_2 VARCHAR(100) NOT NULL,
    dateCreate_2 DATETIME NOT NULL,
    userModif_2 VARCHAR(100) NOT NULL,
    dateModif_2 DATETIME NOT NULL,
-   PRIMARY KEY(IdIgredient, IdProduit),
-   FOREIGN KEY(IdIgredient) REFERENCES Ingredients(IdIgredient),
-   FOREIGN KEY(IdProduit) REFERENCES Produits(IdProduit)
+   PRIMARY KEY(IdIgredient, IdProduct),
+   FOREIGN KEY(IdIgredient) REFERENCES Ingredients(IdIgredient), /* here FK ? */
+   FOREIGN KEY(IdProduct) REFERENCES Products(IdProduct)
 );
 
-CREATE TABLE supplements(
-   IdIgredient INT,
-   IdLignePanier INT,
-   qteSup INT NOT NULL,
-   ajouterSupON LOGICAL NOT NULL,
+CREATE OR REPLACE TABLE Supplements(
+   IdIgredient BIGINT,
+   IdLineBasket BIGINT,
+   qtySup INT NOT NULL,
+   addSupON BOOLEAN NOT NULL,
    userCreate_3 VARCHAR(100) NOT NULL,
    dateCreate_3 DATETIME NOT NULL,
    userModif_3 VARCHAR(100) NOT NULL,
    dateModif_3 DATETIME NOT NULL,
-   PRIMARY KEY(IdIgredient, IdLignePanier),
+   PRIMARY KEY(IdIgredient, IdLineBasket),
    FOREIGN KEY(IdIgredient) REFERENCES Ingredients(IdIgredient),
-   FOREIGN KEY(IdLignePanier) REFERENCES LignePanier(IdLignePanier)
+   FOREIGN KEY(IdLineBasket) REFERENCES LineBasket(IdLineBasket)
 );
 
-CREATE TABLE Occupe(
-   idCommande INT,
-   idEmploye INT,
-   livraisonOccupeON LOGICAL NOT NULL,
-   paiementOccupeON LOGICAL NOT NULL,
+CREATE OR REPLACE TABLE TakeResponsibilityFor(
+   idCommande BIGINT,
+   idEmployee BIGINT,
+   deliveryTakeON BOOLEAN NOT NULL,
+   paymentTakeON BOOLEAN NOT NULL,
    userCreate_1 VARCHAR(100) NOT NULL,
    dateCreate_1 DATETIME NOT NULL,
    userModif_1 VARCHAR(100) NOT NULL,
    dateModif_1 DATETIME NOT NULL,
-   PRIMARY KEY(idCommande, idEmploye),
-   FOREIGN KEY(idCommande) REFERENCES Commandes(idCommande),
-   FOREIGN KEY(idEmploye) REFERENCES Employes(idEmploye)
+   PRIMARY KEY(idCommande, idEmployee),
+   FOREIGN KEY(idCommande) REFERENCES Orders(idCommande),
+   FOREIGN KEY(idEmployee) REFERENCES Employees(idEmployee)
 );
+*/
